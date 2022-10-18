3c4eeddc7a54abae7094c183a58195d122abfa60
yoshuu <appcs342@gmail.com>
2022-10-18 09:37:49 +0800

    1018 02

--- watchers/src/App.vue
+++ watchers/src/App.vue
@@ -1,12 +1,20 @@
 <script setup>
 import { ref, watch } from "vue";
-const text = ref("");
-
-watch(text, (newData, oddData) => {
-  console.log(newData, oddData);
+const text = ref({
+  levels: {
+    text: "",
+  },
 });
+
+watch(
+  () => text.value.levels.text,
+  (newData, oddData) => {
+    console.log(newData, oddData);
+  },
+  { deep: true }
+);
 </script>
 
 <template>
-  <input type="text" v-model="text" />
+  <input type="text" v-model="text.levels.text" />
 </template>
