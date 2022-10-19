d13db7695d13e3325956ead08817070aef5babce
yoshuu <appcs342@gmail.com>
2022-10-19 12:00:51 +0800

    1019 01

--- vue-if-else-show/src/App.vue
+++ vue-if-else-show/src/App.vue
@@ -1,19 +1,10 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import apple1 from "./components/apple1.vue";
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <apple1></apple1>
   </main>
 </template>
 
