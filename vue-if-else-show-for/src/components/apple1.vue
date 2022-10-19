3dac088d948fb32b0e0fac6526edc137ece7a55a
yoshuu <appcs342@gmail.com>
2022-10-19 12:12:59 +0800

    資料夾改名

---
+++ vue-if-else-show-for/src/components/apple1.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from "vue";
+
+const view = ref(1);
+
+const change = (data) => {
+  view.value = data;
+};
+</script>
+
+<template>
+  <button v-on:click="change(1)">顯示A區塊</button>
+  <button v-on:click="change(2)">顯示B區塊</button>
+  <div>
+    <p class="container1" v-show="view === 1">A區塊 <input type="text" /></p>
+
+    <p class="container2" v-show="view === 2">B區塊<input type="text" /></p>
+  </div>
+</template>
+ㄉ
+
+<style scoped>
+p {
+  display: inline-block;
+}
+.container1 {
+  padding: 20px;
+  border: 5px solid red;
+}
+.container2 {
+  padding: 20px;
+  border: 5px solid blue;
+}
+</style>
