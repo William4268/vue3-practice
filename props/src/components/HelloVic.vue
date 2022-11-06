6a9d71fba4253f010ce64fa6ed09d732143954a2
yoshuu <appcs342@gmail.com>
2022-11-06 08:29:20 +0800

    使用全域註冊的components

---
+++ props/src/components/HelloVic.vue
@@ -0,0 +1,9 @@
+<script setup>
+console.log("組件 init");
+</script>
+<template>
+  <div>
+    <h1>Hello Vic</h1>
+  </div>
+</template>
+<style scoped></style>
--- props/src/main.js
+++ props/src/main.js
@@ -1,6 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import HelloVic from "./components/HelloVic.vue";
 
-import './assets/main.css'
+import "./assets/main.css";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.component("HelloVic", HelloVic);
+
+app.mount("#app");
