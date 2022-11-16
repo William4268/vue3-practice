0a1e98ff993fe8fa77679e5cbb6f9c7a1d1684a3
yoshuu <appcs342@gmail.com>
2022-11-16 16:27:44 +0800

    1116 01

---
+++ 123/src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+
+$primary: #a63f24; //主色 $bg-grey: #f8f9fa; // 背景 $font-100: #7a7e83; //
+後台檔名 file-gary $font-200: #23272b; //文字 dark-grey $grey-100: #707070; //
+後台縮圖border
