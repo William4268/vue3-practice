4128892a58eac035579215208a599b687e61582f
yoshuu <appcs342@gmail.com>
2022-11-18 13:08:46 +0800

    1118 02

--- time/src/App.vue
+++ time/src/App.vue
@@ -1,47 +1,51 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive } from "vue";
+
+//取得時間
+let nowTime = reactive({
+  data: "",
+});
+let myDate = new Date();
+function setTime(myDate) {
+  const year = myDate.getFullYear();
+  const month =
+    myDate.getMonth() + 1 < 10
+      ? "0" + (myDate.getMonth() + 1)
+      : myDate.getMonth() + 1;
+  const date =
+    myDate.getDate() < 10 ? "0" + myDate.getDate() : myDate.getDate();
+  const h = myDate.getHours();
+  const m =
+    myDate.getMinutes() < 10 ? "0" + myDate.getMinutes() : myDate.getMinutes();
+  const s =
+    myDate.getSeconds() < 10 ? "0" + myDate.getSeconds() : myDate.getSeconds();
+  nowTime.data =
+    (year - 2020) * 365 +
+    (month - 2) * 30 +
+    (date - 1) +
+    "天" +
+    h +
+    "時" +
+    m +
+    "分" +
+    s +
+    "秒";
+}
+function nowTimes() {
+  setTime(myDate);
+  setInterval(() => {
+    myDate = new Date();
+    setTime(myDate);
+  }, 1000);
+}
+nowTimes();
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>佳憲加油</h1>
+  <h1>模擬的:上任第100天14時2分30秒</h1>
+  <h3>什麼時候上任的？ 2020 2/1 00:00</h3>
+  <h2>上任第{{ nowTime.data }}</h2>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
---
+++ time/src/components/love.vue
@@ -0,0 +1,61 @@
+<script>
+import { reactive, ref } from "vue";
+
+export default {
+  name: "mainTheme",
+  setup() {
+    //取得時間
+    let nowTime = reactive({
+      data: "",
+    });
+    let myDate = new Date();
+    function setTime(myDate) {
+      const year = myDate.getFullYear();
+      const month =
+        myDate.getMonth() + 1 < 10
+          ? "0" + (myDate.getMonth() + 1)
+          : myDate.getMonth() + 1;
+      const date =
+        myDate.getDate() < 10 ? "0" + myDate.getDate() : myDate.getDate();
+      const h = myDate.getHours();
+      const m =
+        myDate.getMinutes() < 10
+          ? "0" + myDate.getMinutes()
+          : myDate.getMinutes();
+      const s =
+        myDate.getSeconds() < 10
+          ? "0" + myDate.getSeconds()
+          : myDate.getSeconds();
+      const time = h + ":" + m + ":" + s;
+      const day = year + "-" + month + "-" + date;
+
+      const apple = s;
+
+      nowTime.data = s + "今天天氣好" + (h - 1);
+
+      const book = m;
+    }
+    function nowTimes() {
+      setTime(myDate);
+      setInterval(() => {
+        myDate = new Date();
+        setTime(myDate);
+      }, 1000);
+    }
+    nowTimes();
+    return {
+      nowTime,
+    };
+  },
+};
+</script>
+
+<template>
+  <love />
+  <h1>佳憲加油</h1>
+  <h2>{{ nowTime.data }}</h2>
+
+  <h2>測試測試{{ year }}</h2>
+</template>
+
+<style scoped></style>
