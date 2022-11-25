fed860b9f890e1dadb3a5534bce960a55af31a59
yoshuu <appcs342@gmail.com>
2022-11-25 08:20:59 +0800

    pinia部署環境

---
+++ pinia/src/App.vue
@@ -0,0 +1,65 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
+
+import { useCounterStore } from "./stores/counter";
+
+const store = useCounterStore();
+
+const book = () => {
+  store.increment();
+};
+</script>
+
+<template>
+  <h1>apple</h1>
+
+  <button @click="book">神奇的小按鈕</button>
+  <h1>{{ store.count }}</h1>
+  <header>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="./assets/logo.svg"
+      width="125"
+      height="125"
+    />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
